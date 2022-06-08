472bd8446387cc7b64ff7a3b9aab95bb3d44db43
bilguun-zorigt <bilguun.zorigt.s@gmail.com>
2022-06-08 10:38:49 +0000

    go

--- Go/src/main.go
+++ Go/src/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("BoM Rate Downloader")
+	// Request the HTML page.
+	res, err := http.Get("https://www.mongolbank.mn/dblistofficialdailyrate.aspx?vYear=2022&vMonth=2&vDay=7")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	parse(res)
+
+}
+
+func parse(httpResponse *http.Response) {
+	doc, err := goquery.NewDocumentFromReader(httpResponse.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	span_id_prefix := "ContentPlaceHolder1_lbl"
+
+	elements := doc.Find(fmt.Sprintf(".uk-comment-list span[id^=%v]", span_id_prefix))
+
+	elements.Each(func(_ int, element *goquery.Selection) {
+		spanId, _ := element.Attr("id")
+		symbol := strings.ReplaceAll(spanId, span_id_prefix, "")
+		rate := strings.ReplaceAll(element.Text(), ",", "")
+
+		fmt.Println(symbol, rate)
+	})
 }
