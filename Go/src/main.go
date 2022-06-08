038d2b14d9db97b5873c6d7c4b94c6a20dfa2c6a
bilguun-zorigt <bilguun.zorigt.s@gmail.com>
2022-06-08 16:05:09 +0000

    go

--- Go/src/main.go
+++ Go/src/main.go
@@ -5,13 +5,37 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
-	// Request the HTML page.
-	res, err := http.Get("https://www.mongolbank.mn/dblistofficialdailyrate.aspx?vYear=2022&vMonth=2&vDay=7")
+	var startDateString string
+	fmt.Println("Enter date to download from (yyyy-mm-dd): ")
+	fmt.Scan(&startDateString)
+	start, _ := time.Parse("2006-01-02", startDateString)
+
+	var endDateString string
+	fmt.Println("Enter date to download to (yyyy-mm-dd): ")
+	fmt.Scan(&endDateString)
+	end, _ := time.Parse("2006-01-02", endDateString)
+
+	for d := start; !d.After(end); d = d.AddDate(0, 0, 1) {
+		wg.Add(1)
+		go request(d)
+	}
+	wg.Wait()
+}
+
+func request(date time.Time) {
+	url_params := fmt.Sprintf("?vYear=%v&vMonth=%v&vDay=%v", date.Year(), int(date.Month()), date.Day())
+	url := "https://www.mongolbank.mn/dblistofficialdailyrate.aspx" + url_params
+
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,11 +43,12 @@ func main() {
 	if res.StatusCode != 200 {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
-	parse(res)
 
+	parse(res, date)
+	wg.Done()
 }
 
-func parse(httpResponse *http.Response) {
+func parse(httpResponse *http.Response, date time.Time) {
 	doc, err := goquery.NewDocumentFromReader(httpResponse.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -33,11 +58,13 @@ func parse(httpResponse *http.Response) {
 
 	elements := doc.Find(fmt.Sprintf(".uk-comment-list span[id^=%v]", span_id_prefix))
 
+	row := map[string]string{"Date": date.Format("2006-01-02")}
 	elements.Each(func(_ int, element *goquery.Selection) {
 		spanId, _ := element.Attr("id")
 		symbol := strings.ReplaceAll(spanId, span_id_prefix, "")
 		rate := strings.ReplaceAll(element.Text(), ",", "")
 
-		fmt.Println(symbol, rate)
+		row[symbol] = rate
 	})
+	fmt.Println(row)
 }
