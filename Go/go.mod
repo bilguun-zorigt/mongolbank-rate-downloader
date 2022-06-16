73762a056f7d7c9af372e0377ef76e22e5a2e7a0
bilguun-zorigt <bilguun.zorigt.s@gmail.com>
2022-06-16 10:16:59 +0800

    add color to windows

--- Go/go.mod
+++ Go/go.mod
@@ -7,4 +7,5 @@ require github.com/PuerkitoBio/goquery v1.8.0
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 )
--- Go/src/main.go
+++ Go/src/main.go
@@ -7,7 +7,17 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"os"
+	"golang.org/x/sys/windows"
 )
+
+func init() {
+    stdout := windows.Handle(os.Stdout.Fd())
+    var originalMode uint32
+
+    windows.GetConsoleMode(stdout, &originalMode)
+    windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+}
 
 var startDate time.Time
 var endDate time.Time
