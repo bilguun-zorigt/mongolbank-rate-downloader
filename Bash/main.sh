d5190b1fa8b9d9706a71cb5ae7b9e0814be708f5
bilguun-zorigt <bilguun.zorigt.s@gmail.com>
2022-07-04 23:26:29 +0800

    bash

---
+++ Bash/main.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# ./scraper.sh
+
+# var startDate time.Time
+# var endDate time.Time
+# var fileName string
+# var totalDays int
+# var doneDays int = 0
+
+function main {
+	welcomeMessage
+
+	datesOrdered=$(getDates)
+    echo $datesOrdered
+	# totalDays = len(datesOrdered)
+
+	# scrapingStartTime := time.Now()
+	# symbolsOrdered, datesSymbolsRates := scrapeConcurrently(datesOrdered, updateProgressBar)
+	# scrapingDuration := time.Since(scrapingStartTime)
+
+	# convertionStartTime := time.Now()
+	# csvString := getCSVString(symbolsOrdered, datesOrdered, datesSymbolsRates)
+	# convertionDuration := time.Since(convertionStartTime)
+
+	# csvWriteStartTime := time.Now()
+	# writeCSVFile(csvString)
+	# csvWriteDuration := time.Since(csvWriteStartTime)
+
+	# successMessage()
+
+	# fmt.Print("\nReports:\n")
+	# printDuration("Scraping:    ", scrapingDuration)
+	# printDuration("Convertion:  ", convertionDuration)
+	# printDuration("CSV creation:", csvWriteDuration)
+}
+
+function welcomeMessage {
+	printf "Source code at: \033[34mhttps://github.com/bilguun-zorigt\033[0m\n"
+}
+
+function getDateInput {
+    local message="$1"
+	while true; do 
+        read -p $'\033[0m'"$message"$'\033[34m' dateString
+		date=$(date -d $dateString +%Y-%m-%d)
+		if [ "$dateString" != "$(date -d "$date" +%Y-%m-%d)" ]; then 
+			echo  $'\033[31mDate entered is not valid. Must be formatted as yyyy-mm-dd\033[0m' > $(tty)
+			continue
+        fi
+        echo $date
+        break
+	done
+}
+
+function getDates {
+	dates=()
+    startDate=$(getDateInput "Enter start date (yyyy-mm-dd): ")
+	endDate=$(getDateInput "Enter end date (yyyy-mm-dd): ")
+    d=
+    n=0
+    until [ "$d" = "$endDate" ]; do  
+        d=$(date -d "$startDate + $n days" +%Y-%m-%d)
+		dates+=($d)
+        echo "$d" > $(tty)
+        ((n++))
+    done
+    for a in $dates; do
+        echo $a > $(tty)
+    done
+	echo $dates
+}
+
+# func updateProgressBar() {
+# 	doneDays++
+# 	pbLength := 25
+# 	doneLength := int(float32(doneDays) * (float32(pbLength) / float32(totalDays)))
+# 	doneLengthString := strings.Repeat("█", doneLength)
+# 	remainingLengthString := strings.Repeat("█", pbLength-doneLength)
+# 	percentString := strconv.FormatFloat(float64(doneDays)/float64(totalDays)*100, 'f', 1, 64)
+# 	// https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+# 	fmt.Printf("\r\033[0mCurrent progress: \033[34m%v\033[41;31m%v\033[0m\033[34m %v/%v %v%%\033[0m", doneLengthString, remainingLengthString, doneDays, totalDays, percentString)
+# }
+
+# func getCSVString(symbolsOrdered []string, datesOrdered []time.Time, datesSymbolsRates datesSymbolsRatesType) string {
+# 	var csvString = "Date"
+# 	for _, symbol := range symbolsOrdered {
+# 		csvString = csvString + "," + symbol
+# 	}
+# 	for _, date := range datesOrdered {
+# 		dateString := date.Format("2006-01-02")
+# 		csvString = csvString + "\n" + dateString
+# 		for _, symbol := range symbolsOrdered {
+# 			rateString := strconv.FormatFloat(datesSymbolsRates[date][symbol], 'f', -1, 64)
+# 			if rateString == "0" {
+# 				rateString = ""
+# 			}
+# 			csvString = csvString + "," + rateString
+# 		}
+# 	}
+# 	return csvString
+# }
+
+# func writeCSVFile(csvString string) {
+# 	fileName = fmt.Sprintf("BoM Rates %v-%v.csv", startDate.Format("20060102"), endDate.Format("20060102"))
+# 	err := ioutil.WriteFile(fileName, []byte(csvString), 0644)
+# 	if err != nil {
+# 		log.Fatal(err)
+# 	}
+# }
+
+# func successMessage() {
+# 	fmt.Printf("\nFile saved: '\033[34m%v\033[0m'\n", fileName)
+# }
+
+# func printDuration(operationName string, durationTime time.Duration) {
+# 	fmt.Printf("%v \033[34m%.2f\033[0m seconds or \033[34m%v\033[0m milliseconds or \033[34m%v\033[0m microseconds or \033[34m%v\033[0m nanoseconds\n", operationName, durationTime.Seconds(), durationTime.Milliseconds(), durationTime.Microseconds(), durationTime.Nanoseconds())
+# }
+
+main
---
+++ Bash/scraper.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+main(){
+    for i in {1..10}; do ppp & done
+    wait
+    echo "1"
+}
+
+function ppp {
+    html=$(curl "https://www.mongolbank.mn/dblistofficialdailyrate.aspx?vYear=2022&vMonth=2&vDay=7" | hxnormalize -x)
+
+    elements=$(echo $html | hxselect -s '\n' ".uk-comment-list span[id^=ContentPlaceHolder1_lbl]")
+
+    old_IFS=$IFS
+    IFS=$'\n'
+
+    str="Date"
+    for e in $elements; do
+        id=$(echo $e | hxselect -c '::attr(id)')
+        id=${id//ContentPlaceHolder1_lbl/}
+        # rate=$(echo $e | hxselect -c 'span')
+        # rate=${rate//,/}
+        str+=",$id"
+        # echo "$id $rate" >> "BoM Rates $(date +%Y%m%d)-$(date +%Y%m%d).csv"
+    done
+    echo $str  >> "BoM Rates $(date +%Y%m%d)-$(date +%Y%m%d).csv"
+
+    str=$(date +%Y-%m-%d)
+    for e in $elements; do
+        # id=$(echo $e | hxselect -c '::attr(id)')
+        # id=${id//ContentPlaceHolder1_lbl/}
+        rate=$(echo $e | hxselect -c 'span')
+        rate=${rate//,/}
+        str+=",$rate"
+        # echo "$id $rate" >> "BoM Rates $(date +%Y%m%d)-$(date +%Y%m%d).csv"
+    done
+    echo $str  >> "BoM Rates $(date +%Y%m%d)-$(date +%Y%m%d).csv"
+
+    IFS=$old_IFS
+}
+
+
+
+
+
+
+# type symbolsRatesType = map[string]float64
+# type datesSymbolsRatesType = map[time.Time]symbolsRatesType
+
+# var symbolsOrdered []string
+# var symbols = make(map[string]bool)
+# var datesSymbolsRates = make(datesSymbolsRatesType)
+# var goRoutineWaitGroup = sync.WaitGroup{}
+
+# func scrapeConcurrently(dates []time.Time, progressCallback func()) ([]string, datesSymbolsRatesType) {
+# 	for _, date := range dates {
+# 		goRoutineWaitGroup.Add(1)
+# 		go request(date, progressCallback)
+# 	}
+# 	goRoutineWaitGroup.Wait()
+# 	return symbolsOrdered, datesSymbolsRates
+# }
+
+# func request(date time.Time, progressCallback func()) {
+# 	urlParams := fmt.Sprintf("?vYear=%v&vMonth=%v&vDay=%v", date.Year(), int(date.Month()), date.Day())
+# 	url := "https://www.mongolbank.mn/dblistofficialdailyrate.aspx" + urlParams
+
+# 	response, err := http.Get(url)
+# 	if err != nil {
+# 		log.Fatal(err)
+# 	}
+# 	defer response.Body.Close()
+# 	if response.StatusCode != 200 {
+# 		log.Fatalf("status code error: %d %s", response.StatusCode, response.Status)
+# 	}
+# 	doc, err := goquery.NewDocumentFromReader(response.Body)
+# 	if err != nil {
+# 		log.Fatal(err)
+# 	}
+
+# 	parse(date, doc)
+# 	progressCallback()
+# 	goRoutineWaitGroup.Done()
+# }
+
+# func parse(date time.Time, doc *goquery.Document) {
+# 	spanIDPrefix := "ContentPlaceHolder1_lbl"
+
+# 	elements := doc.Find(fmt.Sprintf(".uk-comment-list span[id^=%v]", spanIDPrefix))
+
+# 	var symbolsRates = make(symbolsRatesType)
+
+# 	elements.Each(func(_ int, element *goquery.Selection) {
+# 		spanId, _ := element.Attr("id")
+# 		symbol := strings.ReplaceAll(spanId, spanIDPrefix, "")
+# 		if value := symbols[symbol]; !value {
+# 			symbols[symbol] = true
+# 			symbolsOrdered = append(symbolsOrdered, symbol)
+# 		}
+# 		if element.Text() != "" {
+# 			rate, err := strconv.ParseFloat(strings.ReplaceAll(element.Text(), ",", ""), 64)
+# 			if err == nil {
+# 				symbolsRates[symbol] = rate
+# 			}
+# 		}
+# 	})
+# 	datesSymbolsRates[date] = symbolsRates
+# }
