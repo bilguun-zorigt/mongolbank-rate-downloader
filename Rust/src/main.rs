5f4f91dcb5b0d78d38f17f3ddc746fc4be477a3b
bilguun-zorigt <bilguun.zorigt.s@gmail.com>
2022-08-14 03:09:22 +0000

    rust

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,49 +1,140 @@
-// use reqwest::{self, header};
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let mut headers = header::HeaderMap::new();
-//     headers.insert(
-//         header::USER_AGENT,
-//         header::HeaderValue::from_static("Mozilla/5.0...."),
-//     );
+mod scrape;
 
-//     let client = reqwest::Client::builder()
-//         .default_headers(headers)
-//         .build()?;
+use chrono::{Duration, NaiveDate};
+// use std::sync::{Arc, Mutex};
+use std::{
+    io::{stdin, Write},
+    str::FromStr,
+};
 
-//     let res = client
-//         .get("https://www.mongolbank.mn/dblistofficialdailyrate.aspx?vYear=2022&vMonth=2&vDay=7")
-//         .send()
-//         .await?;
-//     println!("{:#?}", res.text().await);
-//     Ok(())
-// }
+// let start_date=chrono::NaiveDate;
+// let end_date= time.Time
+// var fileName string
+// var totalDays int
+// var doneDays int = 0
 
-// }
-extern crate reqwest;
+fn main() {
+    welcome_message();
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://www.mongolbank.mn/dblistofficialdailyrate.aspx?vYear=2022&vMonth=2&vDay=7";
+    let dates_ordered = get_dates();
+    let total_days = dates_ordered.len();
+    println!("First: {:#?}\nSecond: {:#?}", dates_ordered, total_days);
 
-    let client = reqwest::Client::new();
-    let response = client
-        .get(url)
-        .header("User-Agent", "Mozilla/5.0....")
-        .send()
-        .await? // ? == .unwrap()
-        .text()
-        .await?; // ? == .unwrap()
+    // scraping_start_time := time.Now()
+    let dates_symbols_rates = scrape::scrape_concurrently(dates_ordered, &update_progress_bar);
+    println!("Second: {:#?}", dates_symbols_rates);
+    // let (symbols_ordered, dates_symbols_rates) = scrape_concurrently(dates_ordered, update_progress_bar);
+    // scraping_duration := time.Since(scraping_start_time)
 
-    let document = scraper::Html::parse_document(&response);
+    // convertion_start_time := time.Now()
+    // csv_string := get_csv_string(symbols_ordered, dates_ordered, dates_symbols_rates)
+    // convertion_duration := time.Since(convertion_start_time)
 
-    let title_selector = scraper::Selector::parse(".uk-comment-list span").unwrap();
+    // csv_write_start_time := time.Now()
+    // write_csv_file(csvString)
+    // csv_write_duration := time.Since(csv_write_start_time)
 
-    let titles = document
-        .select(&title_selector)
-        .map(|x| (x.value().attr("id").unwrap(), x.inner_html()));
+    // success_message();
 
-    titles.for_each(|(id, rate)| println!("{} {}", id, rate));
-
-    Ok(())
+    // fmt.Print("\nReports:\n")
+    // printDuration("Scraping:    ", scraping_duration)
+    // printDuration("Convertion:  ", convertion_duration)
+    // printDuration("CSV creation:", csv_write_duration)
 }
+
+fn welcome_message() {
+    println!("Source code at: \x1b[34mhttps://github.com/bilguun-zorigt\x1b[0m")
+}
+
+fn get_date_input(message: &str) -> chrono::NaiveDate {
+    loop {
+        print!("\x1b[0m{}\x1b[34m", message);
+        std::io::stdout().flush().expect("");
+        let mut date_string = String::new();
+        stdin()
+            .read_line(&mut date_string)
+            .expect("Failed to read line");
+
+        let date = NaiveDate::from_str(&date_string);
+
+        match date {
+            Ok(date) => return date,
+            Err(_) => {
+                println!(
+                    "\x1b[31mDate entered is not valid. Must be formatted as yyyy-mm-dd\x1b[0m"
+                );
+                continue;
+            }
+        }
+    }
+}
+
+fn get_dates() -> Vec<chrono::NaiveDate> {
+    let start_date = get_date_input("Enter start date (yyyy-mm-dd): ");
+    let end_date = get_date_input("Enter end date (yyyy-mm-dd): ");
+
+    let mut dates: Vec<chrono::NaiveDate> = Vec::new();
+    let mut date = start_date;
+    while date <= end_date {
+        dates.push(date);
+        date += Duration::days(1);
+    }
+    dates
+}
+
+// static counter: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
+
+fn update_progress_bar() {
+    // let counter = Arc::clone(&counter);
+    // let mut num = counter.lock().unwrap();
+    // *num += 1;
+
+    // println!("Result: {}", *counter.lock().unwrap());
+
+    println!("One down");
+
+    // let doneDays += 1;
+    // println!("{:?}", doneDays);
+    // pbLength := 25
+    // doneLength := int(float32(doneDays) * (float32(pbLength) / float32(totalDays)))
+    // doneLengthString := strings.Repeat("█", doneLength)
+    // remainingLengthString := strings.Repeat("█", pbLength-doneLength)
+    // percentString := strconv.FormatFloat(float64(doneDays)/float64(totalDays)*100, 'f', 1, 64)
+    // // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    // fmt.Printf("\r\x1b[0mCurrent progress: \x1b[34m%v\x1b[41;31m%v\x1b[0m\x1b[34m %v/%v %v%%\x1b[0m", doneLengthString, remainingLengthString, doneDays, totalDays, percentString)
+}
+
+// func get_csv_string(symbolsOrdered []string, datesOrdered []time.Time, datesSymbolsRates datesSymbolsRatesType) string {
+// 	var csvString = "Date"
+// 	for _, symbol := range symbolsOrdered {
+// 		csvString = csvString + "," + symbol
+// 	}
+// 	for _, date := range datesOrdered {
+// 		dateString := date.Format("2006-01-02")
+// 		csvString = csvString + "\n" + dateString
+// 		for _, symbol := range symbolsOrdered {
+// 			rateString := strconv.FormatFloat(datesSymbolsRates[date][symbol], 'f', -1, 64)
+// 			if rateString == "0" {
+// 				rateString = ""
+// 			}
+// 			csvString = csvString + "," + rateString
+// 		}
+// 	}
+// 	return csvString
+// }
+
+// func writeCSVFile(csvString string) {
+// 	fileName = fmt.Sprintf("BoM Rates %v-%v.csv", start_date.Format("20060102"), end_date.Format("20060102"))
+// 	err := ioutil.WriteFile(fileName, []byte(csvString), 0644)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+// fn success_message() {
+//     println!("\nFile saved: '\x1b[34m{:#}\x1b[0m'\n", fileName)
+// }
+
+// func printDuration(operationName string, durationTime time.Duration) {
+// 	fmt.Printf("%v \x1b[34m%.2f\x1b[0m seconds or \x1b[34m%v\x1b[0m milliseconds or \x1b[34m%v\x1b[0m microseconds or \x1b[34m%v\x1b[0m nanoseconds\n", operationName, durationTime.Seconds(), durationTime.Milliseconds(), durationTime.Microseconds(), durationTime.Nanoseconds())
+// }
---
+++ Rust/src/scrape.rs
@@ -0,0 +1,74 @@
+use chrono::Datelike;
+use futures::future::join_all;
+use linked_hash_map::LinkedHashMap;
+
+#[tokio::main]
+pub async fn scrape_concurrently(
+    dates: Vec<chrono::NaiveDate>,
+    progress_callback: &dyn Fn(),
+) -> LinkedHashMap<chrono::NaiveDate, LinkedHashMap<String, f64>> {
+    let mut requests = Vec::new();
+    for date in dates {
+        requests.push(request(date, progress_callback))
+    }
+
+    let results = join_all(requests).await;
+
+    let mut dates_symbols_rates: LinkedHashMap<chrono::NaiveDate, LinkedHashMap<String, f64>> =
+        LinkedHashMap::new();
+    for (k, v) in results {
+        dates_symbols_rates.insert(k, v);
+    }
+    dates_symbols_rates
+}
+
+async fn request(
+    date: chrono::NaiveDate,
+    progress_callback: &dyn Fn(),
+) -> (chrono::NaiveDate, LinkedHashMap<String, f64>) {
+    let url = format!(
+        "https://www.mongolbank.mn/dblistofficialdailyrate.aspx?vYear={}&vMonth={}&vDay={}",
+        date.year(),
+        date.month(),
+        date.day()
+    );
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .header("User-Agent", "Mozilla/5.0....")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let doc = scraper::Html::parse_document(&response);
+    let symbols_rates = parse(doc);
+    println!("{:?}", symbols_rates);
+    progress_callback();
+    (date, symbols_rates)
+}
+
+fn parse(doc: scraper::Html) -> LinkedHashMap<String, f64> {
+    const SPAN_ID_PREFIX: &str = "ContentPlaceHolder1_lbl";
+    let selector_str = format!(".uk-comment-list span[id^={}]", SPAN_ID_PREFIX);
+    let elements_selector = scraper::Selector::parse(selector_str.as_str()).unwrap();
+    let elements = doc.select(&elements_selector);
+
+    let mut symbols_rates: LinkedHashMap<String, f64> = LinkedHashMap::new();
+    for element in elements {
+        let symbol = element
+            .value()
+            .attr("id")
+            .unwrap()
+            .replace(SPAN_ID_PREFIX, "");
+
+        if let Ok(rate) = element.inner_html().replace(',', "").parse::<f64>() {
+            symbols_rates.insert(symbol, rate);
+        }
+    }
+
+    symbols_rates
+}
